3e6affcaa207966ee3b431eb8a66d6d1674f6e9c
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-01-22 23:03:47 +0100

    check time-table

--- time-tracking-dashboard/js/store.js
+++ time-tracking-dashboard/js/store.js
@@ -1,11 +1,8 @@
 var result;
 
 fetch('data.json').then(function(res){
-    //console.log(res.json());
     res.json().then(function(data){
-        console.log(data);
         result = data;
-        console.log(result);
         return result
     });
 });
@@ -31,8 +28,36 @@ const fetchData = () => {
     daily.addEventListener('click', displayDaily);
 
     const displayWeekly = () => {
-        
+        document.getElementById('work').innerHTML = result[0]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('workPrev').innerHTML = "Last week - " + result[0]['timeframes']['weekly']['previous'] + 'hrs';
+        document.getElementById('play').innerHTML = result[1]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('playPrev').innerHTML = "Last week - " + result[1]['timeframes']['weekly']['previous'] + 'hrs';
+        document.getElementById('study').innerHTML = result[2]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('studyPrev').innerHTML = "Last week - " + result[2]['timeframes']['weekly']['previous'] + 'hrs';
+        document.getElementById('exercise').innerHTML = result[3]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('exercisePrev').innerHTML = "Last week - " + result[3]['timeframes']['weekly']['previous'] + 'hrs';
+        document.getElementById('social').innerHTML = result[4]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('socialPrev').innerHTML = "Last week - " + result[4]['timeframes']['weekly']['previous'] + 'hrs';
+        document.getElementById('selfcare').innerHTML = result[5]['timeframes']['weekly']['current'] + 'hrs';
+        document.getElementById('selfcarePrev').innerHTML = "Last week - " + result[5]['timeframes']['weekly']['previous'] + 'hrs';
     }
-    var week = document.getElementById('daily');
+    var week = document.getElementById('weekly');
     week.addEventListener('click', displayWeekly);
+
+    const displayMonthly = () => {
+        document.getElementById('work').innerHTML = result[0]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('workPrev').innerHTML = "Last week - " + result[0]['timeframes']['monthly']['previous'] + 'hrs';
+        document.getElementById('play').innerHTML = result[1]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('playPrev').innerHTML = "Last week - " + result[1]['timeframes']['monthly']['previous'] + 'hrs';
+        document.getElementById('study').innerHTML = result[2]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('studyPrev').innerHTML = "Last week - " + result[2]['timeframes']['monthly']['previous'] + 'hrs';
+        document.getElementById('exercise').innerHTML = result[3]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('exercisePrev').innerHTML = "Last week - " + result[3]['timeframes']['monthly']['previous'] + 'hrs';
+        document.getElementById('social').innerHTML = result[4]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('socialPrev').innerHTML = "Last week - " + result[4]['timeframes']['monthly']['previous'] + 'hrs';
+        document.getElementById('selfcare').innerHTML = result[5]['timeframes']['monthly']['current'] + 'hrs';
+        document.getElementById('selfcarePrev').innerHTML = "Last week - " + result[5]['timeframes']['monthly']['previous'] + 'hrs';
+    }
+    var month = document.getElementById('month');
+    month.addEventListener('click', displayMonthly);
 }
